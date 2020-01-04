5feab48e4fbc6aa87f162aa08e823d28b36c5404
gmlander <garrettlander@gmail.com>
2020-01-03 20:11:38 -0800

    added board update logic

--- utils.py
+++ utils.py
@@ -17,6 +17,8 @@ class Board():
 			for y in range(self.l):
 				self.board[x][y] = self.get_num_mines(x,y)
 
+		self.display = np.zeros((self.w, self.l), dtype = bool)
+
 	def get_num_mines(self, x, y):
 		if (x,y) in self.mines:
 			return -1
@@ -31,3 +33,32 @@ class Board():
 			return 0
 		else:
 			return (x_adj, y_adj) in self.mines
+
+	def update_square(self, pick_mine, x, y)
+		if (x,y) in self.mines:
+			if pick_mine:
+				self.display[x][y] = True
+				return True
+			else:
+				return False
+		else:
+			if (x,y) in self.mines:
+				return False
+			else:
+				self.update_adjacents(x,y)
+				return True
+
+	def update_adjacents(self, x, y):
+		'''recursively frees up display on adjacent squares when clear
+		cell is selected'''
+
+		if self.board[x][y] == 0:
+			self.display[x][y] = True
+			[update_adjacents(*c) for c in product([x - 1, x, x + 1],[y - 1, y, y + 1])]
+				# if self.board[cx][cy] 
+				# adj_result = self.update_adjacents(*c)
+			return True
+
+		else self.board[x][y] > 0:
+			self.display[x][y] = True
+			return False
