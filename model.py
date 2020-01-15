f0bc6af5ade301a63c3872b49e5dcfbe762482b0
gmlander <garrettlander@gmail.com>
2020-01-14 18:05:34 -0800

    commented out model.py and train.py to compensate for repl.it's auto-dependency builder

--- model.py
+++ model.py
@@ -1,115 +1,115 @@
-import tensorflow as tf
-import tensorflow.keras.layers as layers
-from tensorflow.keras.models import Model
+# import tensorflow as tf
+# import tensorflow.keras.layers as layers
+# from tensorflow.keras.models import Model
 
-NUM_POS_FEATS = 8
-NUM_GAME_FEATS = 4
+# NUM_POS_FEATS = 8
+# NUM_GAME_FEATS = 4
 
-tf.set_random_seed(42)
+# tf.set_random_seed(42)
 
-def input_fn(is_training, features, labels, batch_size = 256):
+# def input_fn(is_training, features, labels, batch_size = 256):
     
-    def get_fields(record_feats, record_label):
-        # Define features
-        neighbors, game_feats = record_feats
+#     def get_fields(record_feats, record_label):
+#         # Define features
+#         neighbors, game_feats = record_feats
 
-        feats = {'neighbors': neighbors,
-                'game_feats': game_feats}
+#         feats = {'neighbors': neighbors,
+#                 'game_feats': game_feats}
         
-        return feats, [record_label]
+#         return feats, [record_label]
 
         
-    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
-    dataset = dataset.shuffle(5000, reshuffle_each_iteration=True).repeat()
-    dataset = dataset.map(map_func = get_fields,
-                            num_parallel_calls = tf.data.experimental.AUTOTUNE).batch(batch_size = batch_size,
-                            drop_remainder=True)
-    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
-    return dataset
+#     dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+#     dataset = dataset.shuffle(5000, reshuffle_each_iteration=True).repeat()
+#     dataset = dataset.map(map_func = get_fields,
+#                             num_parallel_calls = tf.data.experimental.AUTOTUNE).batch(batch_size = batch_size,
+#                             drop_remainder=True)
+#     dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
+#     return dataset
 
-def create_model(**kwargs):
-    """Creates a tf.Keras Model for page type and relevance classification.
+# def create_model(**kwargs):
+#     """Creates a tf.Keras Model for page type and relevance classification.
 
-    Returns:
-    The compiled Keras model (still needs to be trained)
-    """
-    low_drop = kwargs.setdefault('drop_rate', .3)
-    pred_drop = low_drop/2
+#     Returns:
+#     The compiled Keras model (still needs to be trained)
+#     """
+#     low_drop = kwargs.setdefault('drop_rate', .3)
+#     pred_drop = low_drop/2
 
 
-    if kwargs.setdefault('activation_type', 'selu') == 'selu':
-        K_INIT = 'lecun_normal'
-        DROPOUT_LAYER = layers.AlphaDropout
-    else:
-        K_INIT = 'glorot_uniform'
-        DROPOUT_LAYER = layers.Dropout
+#     if kwargs.setdefault('activation_type', 'selu') == 'selu':
+#         K_INIT = 'lecun_normal'
+#         DROPOUT_LAYER = layers.AlphaDropout
+#     else:
+#         K_INIT = 'glorot_uniform'
+#         DROPOUT_LAYER = layers.Dropout
 
-    #########################################################################
-    #################### BUILD MODEL ########################################
-    #########################################################################
+#     #########################################################################
+#     #################### BUILD MODEL ########################################
+#     #########################################################################
 
-    ############################## INPUTS ########################################
-    ##### sentence level inputs
+#     ############################## INPUTS ########################################
+#     ##### sentence level inputs
 
-    # 8 positional features
-    # tl, t, tr, r, br, b, bl, l
-    # -2 = unknown
-    # -1 = mine
-    # 0 = clear
-    # 1-6 = number of mines
-    # semi - 1 hot encoded in form of (unknown, mine, number of mines) - (0/1, 0/1, 0-6)
-    neighbors = layers.Input(shape=(NUM_POS_FEATS, 3), dtype = tf.int64, name = 'neighbors')
+#     # 8 positional features
+#     # tl, t, tr, r, br, b, bl, l
+#     # -2 = unknown
+#     # -1 = mine
+#     # 0 = clear
+#     # 1-6 = number of mines
+#     # semi - 1 hot encoded in form of (unknown, mine, number of mines) - (0/1, 0/1, 0-6)
+#     neighbors = layers.Input(shape=(NUM_POS_FEATS, 3), dtype = tf.int64, name = 'neighbors')
 
-    # game state features
-    # total number of cells, total number of mines, number of known cells, number of known mines
-    game_feats = layers.Input(shape=(NUM_GAME_FEATS,), dtype = tf.int64, name = 'game_feats')
+#     # game state features
+#     # total number of cells, total number of mines, number of known cells, number of known mines
+#     game_feats = layers.Input(shape=(NUM_GAME_FEATS,), dtype = tf.int64, name = 'game_feats')
 
-    ############################## DENSE LAYERS ###############################
+#     ############################## DENSE LAYERS ###############################
 
-    dense_neighbors = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
-                                    activation=kwargs['activation_type'])(neighbors)
+#     dense_neighbors = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
+#                                     activation=kwargs['activation_type'])(neighbors)
 
-    dense_game_feats = layers.Dense(int(kwargs['num_nodes']/4), kernel_initializer=K_INIT,
-                                    activation=kwargs['activation_type'])(game_feats)
+#     dense_game_feats = layers.Dense(int(kwargs['num_nodes']/4), kernel_initializer=K_INIT,
+#                                     activation=kwargs['activation_type'])(game_feats)
 
     
 
-    # create additional layers if needed
-    for _ in range(kwargs['num_pre_merge_layers'] - 1):
-        drop_neighbors = DROPOUT_LAYER(low_drop)(dense_neighbors)
-        drop_game_feats = DROPOUT_LAYER(low_drop)(dense_game_feats)
+#     # create additional layers if needed
+#     for _ in range(kwargs['num_pre_merge_layers'] - 1):
+#         drop_neighbors = DROPOUT_LAYER(low_drop)(dense_neighbors)
+#         drop_game_feats = DROPOUT_LAYER(low_drop)(dense_game_feats)
 
         
-        dense_neighbors = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
-                                    activation= kwargs['activation_type'])(drop_neighbors)
-        dense_game_feats = layers.Dense(int(kwargs['num_nodes']/4), kernel_initializer=K_INIT,
-                                    activation= kwargs['activation_type'])(drop_game_feats)
+#         dense_neighbors = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
+#                                     activation= kwargs['activation_type'])(drop_neighbors)
+#         dense_game_feats = layers.Dense(int(kwargs['num_nodes']/4), kernel_initializer=K_INIT,
+#                                     activation= kwargs['activation_type'])(drop_game_feats)
 
-    ############################## MERGE ###############################
+#     ############################## MERGE ###############################
 
-    merge_feats = layers.concatenate([dense_neighbors, dense_game_feats])
+#     merge_feats = layers.concatenate([dense_neighbors, dense_game_feats])
 
-    #################### ADDITIONAL DENSE LAYERS & PRED ################
+#     #################### ADDITIONAL DENSE LAYERS & PRED ################
 
-    for _ in range(kwargs['num_post_merge_layers'] - 1):
-        drop_merge_feats = DROPOUT_LAYER(pred_drop)(merge_feats)
+#     for _ in range(kwargs['num_post_merge_layers'] - 1):
+#         drop_merge_feats = DROPOUT_LAYER(pred_drop)(merge_feats)
         
-        dense_merge_feats = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
-                                    activation= kwargs['activation_type'])(drop_merge_feats)
+#         dense_merge_feats = layers.Dense(kwargs['num_nodes'], kernel_initializer=K_INIT,
+#                                     activation= kwargs['activation_type'])(drop_merge_feats)
 
 
-    # prediction layer
-    pred = layers.Dense(1, activation='sigmoid', name = 'has_mine')(dense_merge_feats)
+#     # prediction layer
+#     pred = layers.Dense(1, activation='sigmoid', name = 'has_mine')(dense_merge_feats)
 
-    model = Model(inputs = [neighbors,
-                            game_feats],
-                  outputs = pred)
+#     model = Model(inputs = [neighbors,
+#                             game_feats],
+#                   outputs = pred)
 
-    if kwargs.setdefault('optimizer', 'adam') == 'adam':
-        opt = tf.keras.optimizers.Adam(kwargs.setdefault('learning_rate', .001))
-    else:
-        opt = kwargs['optimizer']
+#     if kwargs.setdefault('optimizer', 'adam') == 'adam':
+#         opt = tf.keras.optimizers.Adam(kwargs.setdefault('learning_rate', .001))
+#     else:
+#         opt = kwargs['optimizer']
     
-    model.compile(loss='binary_crossentropy', optimizer= opt)
+#     model.compile(loss='binary_crossentropy', optimizer= opt)
 
-    return model
+#     return model
--- train.py
+++ train.py
@@ -1,76 +1,76 @@
-from itertools import product
-from collections import defaultdict
-import matplotlib.pyplot as plt
+# from itertools import product
+# from collections import defaultdict
+# import matplotlib.pyplot as plt
 
-import tensorflow as tf
-from tensorflow.keras import backend as K
-import os
-import numpy as np
+# import tensorflow as tf
+# from tensorflow.keras import backend as K
+# import os
+# import numpy as np
 
-from utils import Board
-from model import create_model, input_fn
+# from utils import Board
+# from model import create_model, input_fn
 
-MIN_X = 10
-MAX_X = 30
-MIN_Y = 10
-MAX_Y = 30
-NUM_BOARDS = 10
-OBS_PER_BOARD = 10
+# MIN_X = 10
+# MAX_X = 30
+# MIN_Y = 10
+# MAX_Y = 30
+# NUM_BOARDS = 10
+# OBS_PER_BOARD = 10
 
-# MINE_DIST = [0.65641, 0.23598, 0.05507, 0.01501, 0.00972, 0.00929, 0.00926, 0.00926]
-MINE_COORDS = list(product(range(-1, 2), range(-1,2)))
-SHOWN_COORDS = [c for c in MINE_COORDS if c != (0,0)]
+# # MINE_DIST = [0.65641, 0.23598, 0.05507, 0.01501, 0.00972, 0.00929, 0.00926, 0.00926]
+# MINE_COORDS = list(product(range(-1, 2), range(-1,2)))
+# SHOWN_COORDS = [c for c in MINE_COORDS if c != (0,0)]
 
-def build_train_data():
+# def build_train_data():
 
-	game_feats = []
-	game_cells = []
-	cell_labels = []
+# 	game_feats = []
+# 	game_cells = []
+# 	cell_labels = []
 
-	for i in range(MIN_X, MAX_X + 1):
-	    for j in range(MIN_Y, MAX_Y + 1):
-	        for _ in range(NUM_BOARDS):
-	            board = Board(i, j)
-	            squares = product(range(i), range(j))
-	            cells = np.random.choice(squares, size = OBS_PER_BOARD, replace = False)
-	            n_cells = board.w * board.l
-	            n_mines = board.num_mines
-	            for c in cells:
+# 	for i in range(MIN_X, MAX_X + 1):
+# 	    for j in range(MIN_Y, MAX_Y + 1):
+# 	        for _ in range(NUM_BOARDS):
+# 	            board = Board(i, j)
+# 	            squares = product(range(i), range(j))
+# 	            cells = np.random.choice(squares, size = OBS_PER_BOARD, replace = False)
+# 	            n_cells = board.w * board.l
+# 	            n_mines = board.num_mines
+# 	            for c in cells:
 
-	            	cell = game_cell(board, c)
-	                cell_mines = sum([1 for c in cell if c == -1])
-	                cell_shown = sum([1 for c in cell if c >= 0])
-	                mines_known = np.random.randint(cell_mines, n_mines - 1)
-	                cells_known = np.random.randint(cell_shown, n_cells - 1)
+# 	            	cell = game_cell(board, c)
+# 	                cell_mines = sum([1 for c in cell if c == -1])
+# 	                cell_shown = sum([1 for c in cell if c >= 0])
+# 	                mines_known = np.random.randint(cell_mines, n_mines - 1)
+# 	                cells_known = np.random.randint(cell_shown, n_cells - 1)
 
-	                # game state features
-				    # total number of cells, total number of mines, number of known cells, number of known mines
-				    game_feats.append((n_mines, n_cells, cells_known, mines_known))
+# 	                # game state features
+# 				    # total number of cells, total number of mines, number of known cells, number of known mines
+# 				    game_feats.append((n_mines, n_cells, cells_known, mines_known))
 
 
-def game_cell(board, c):
-	# num_mines = np.random.choice(a = [i for i in range(1,9)], p = MINE_DIST)
-	# mines = np.random.choice(a = MINE_COORDS,
-	# 						size = num_mines, replace = False)
+# def game_cell(board, c):
+# 	# num_mines = np.random.choice(a = [i for i in range(1,9)], p = MINE_DIST)
+# 	# mines = np.random.choice(a = MINE_COORDS,
+# 	# 						size = num_mines, replace = False)
 
-	num_shown = np.random.randint(0, 9)
-	shown = np.random.choice(a = SHOWN_COORDS,
-							size = num_shown, replace = False)
+# 	num_shown = np.random.randint(0, 9)
+# 	shown = np.random.choice(a = SHOWN_COORDS,
+# 							size = num_shown, replace = False)
 
-	cell = np.full((3,3), -2)
+# 	cell = np.full((3,3), -2)
 
-	for s in shown:
-		cell[s] = get_num_mines(s, mines, shown)
+# 	for s in shown:
+# 		cell[s] = get_num_mines(s, mines, shown)
 
-	cell = cell.flatten()
-	out_cell = np.delete(cell, 4)
-	return out_cell
+# 	cell = cell.flatten()
+# 	out_cell = np.delete(cell, 4)
+# 	return out_cell
 
-def get_num_mines(s, mines, shown):
+# def get_num_mines(s, mines, shown):
 
-	if s in mines:
-		return -1
-	else:
-		x, y = s
-		return sum([self.get_neighbor(*c) for c in product([x - 1, x, x + 1],
-														[y - 1, y, y + 1])])
+# 	if s in mines:
+# 		return -1
+# 	else:
+# 		x, y = s
+# 		return sum([self.get_neighbor(*c) for c in product([x - 1, x, x + 1],
+# 														[y - 1, y, y + 1])])
